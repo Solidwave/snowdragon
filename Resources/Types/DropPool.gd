bb5dd652619945f83d7426c61897045f9f0e71ac
solidwave <solidwave.dev@gmail.com>
2023-11-16 23:22:14 +0100

    feat: adds trees, adds skeleton, adds basic menu

--- Resources/Types/DropPool.gd
+++ Resources/Types/DropPool.gd
@@ -2,6 +2,7 @@ extends Resource
 
 class_name DropPoolResource
 
+#Note: the rarest items should be at the start of the array
 @export var items: Array[ItemResource] = []
 @export var chances: Array[float] = []
 
@@ -15,5 +16,4 @@ func drop():
 	for i in range(items.size()):
 		if rand < chances[i]:
 			return items[i]
-		rand -= chances[i]
 	return null;
---
+++ UI/Menu/Menu.gd
@@ -0,0 +1,10 @@
+extends Control
+
+
+
+func _on_play_button_pressed():
+	get_tree().change_scene_to_file("res://Scenes/Main/Main.tscn")
+
+
+func _on_exit_button_pressed():
+	get_tree().quit()
