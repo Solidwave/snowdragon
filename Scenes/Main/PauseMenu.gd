471390b9ddc25fa3ab865ab2319ac0007e67a651
solidwave <solidwave.dev@gmail.com>
2023-12-05 00:46:20 +0100

    Adds pause menu, Fixes y sorting

--- Scenes/Main/PauseMenu.gd
+++ Scenes/Main/PauseMenu.gd
@@ -4,27 +4,26 @@ extends MarginContainer
 
 var healthComponent : HealthComponent
 
-func _ready():
-	healthComponent = player.healthComponent
-	
-	if healthComponent:
-		healthComponent.im_dead.connect(showMenu)
-
-func showMenu():
-	visible = true	
-	
-	get_tree().paused = true
-	
 func _on_retry_button_button_up():
+	get_tree().paused = false
+	
 	get_tree().reload_current_scene()
 	
-	get_tree().paused = false
 
 func _on_menu_button_button_up():
 	get_tree().paused = false
 	get_tree().change_scene_to_file('res://UI/Menu/Menu.tscn')
-	
-	
 
 func _on_exit_button_button_up():
 	get_tree().quit()
+
+
+func _on_texture_button_button_up():
+	if get_tree().paused:
+		get_tree().paused = false
+		visible = false
+	else:
+		get_tree().paused = true
+		visible = true
+		
+		
